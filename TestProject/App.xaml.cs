74c95d83a1a092853387f627f3a75750dcdd1bae
Pelle Mathias Rasmussen <pmr@precure.dk>
2024-09-20 08:58:48 +0200

    Add project files.

---
+++ TestProject/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace TestProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
+            {
+                Console.WriteLine("AppDomain");
+            };
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                Console.WriteLine("TaskScheduler");
+            };
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TestProject/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace TestProject
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnDisappearing()
+        {
+            base.OnDisappearing();
+  
+           
+        }
+        
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+           
+            App.Current.MainPage = new MainPage();
+        }
+
+        private void ContentPage_Unloaded(object sender, EventArgs e)
+        {
+            mediaElement?.Handler?.DisconnectHandler();
+        }
+    }
+
+}
